0d85fc677043f2e53652917e36d5302a9f7d1fbb
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-11-12 15:40:40 +0900

    add udp client

---
+++ udpClient.py
@@ -0,0 +1,15 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 80
+
+# create socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send data
+client.sendto("AABBCC",(target_host,target_port))
+
+# reveave data
+data, addr = client.recvfrom(4096)
+
+print response
